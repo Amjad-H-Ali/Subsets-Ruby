8ab2bf35dc2bb1a3ddfeb9010921948464a38c44
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-29 20:45:59 -0500

    Defined function

--- subsets.rb
+++ subsets.rb
@@ -1,1 +1,10 @@
 p "Subsets!"
+
+# Helper Function is Recursive
+# Takes in three arguments:
+# data: the array holding a given set
+# subset: an array containing the subsets
+# i: the current index
+# No return value, only prints to std output.
+def subsets data, subset, i
+end	
