7f91f377e647eef21dfd477ae5f2f63dd6364235
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-29 20:48:21 -0500

    Default value of i param is 0

--- subsets.rb
+++ subsets.rb
@@ -1,11 +1,11 @@
 p "Subsets!"
 
-# Helper Function is Recursive
+# Function is Recursive
 # Takes in three arguments:
 # data: the array holding a given set
 # subset: an array containing the subsets
 # i: the current index
 # No return value, only prints to std output.
-def subsets data, subset=[], i
+def subsets data, subset=[], i=0
 
 end	
